babbc386815e65cfe13eb8c10382df1b024b52e3
peachey <>
2006-01-27 22:08:47 +0000

    Change interface to accept independently the ephemeris epoch and origin. Change body of the application to use the file keywords to set up event times, and the new parameters to set up the eph epoch and the origin time.

--- src/gtpsearch/gtpsearch.cxx
+++ src/gtpsearch/gtpsearch.cxx
@@ -67,6 +67,7 @@ PSearchApp::PSearchApp(): m_os("PSearchApp", "", 2), m_data_dir(), m_test(0) {
 
   pars.setSwitch("ephstyle");
   pars.setSwitch("cancelpdot");
+  pars.setSwitch("originstyle");
   pars.setCase("ephstyle", "FREQ", "f0");
   pars.setCase("ephstyle", "DB", "cancelpdot");
   pars.setCase("ephstyle", "FREQ", "cancelpdot");
@@ -74,11 +75,20 @@ PSearchApp::PSearchApp(): m_os("PSearchApp", "", 2), m_data_dir(), m_test(0) {
   pars.setCase("cancelpdot", "true", "f1");
   pars.setCase("ephstyle", "FREQ", "f1");
   pars.setCase("ephstyle", "FREQ", "f2");
+  pars.setCase("ephstyle", "FREQ", "ephepoch");
+  pars.setCase("ephstyle", "FREQ", "timeformat");
+  pars.setCase("ephstyle", "FREQ", "timesys");
   pars.setCase("ephstyle", "PER", "p0");
   pars.setCase("ephstyle", "PER", "p1");
+  pars.setCase("ephstyle", "PER", "ephepoch");
+  pars.setCase("ephstyle", "PER", "timeformat");
+  pars.setCase("ephstyle", "PER", "timesys");
   pars.setCase("cancelpdot", "true", "p1");
   pars.setCase("ephstyle", "PER", "p2");
   pars.setCase("ephstyle", "DB", "psrname");
+  pars.setCase("originstyle", "USER", "origintime");
+  pars.setCase("originstyle", "USER", "originformat");
+  pars.setCase("originstyle", "USER", "originsys");
 }
 
 PSearchApp::~PSearchApp() throw() {
@@ -97,13 +107,18 @@ void PSearchApp::run() {
   std::string event_extension = pars["evtable"];
   double scan_step = pars["scanstep"];
   long num_trials = pars["numtrials"];
-  double epoch = pars["epoch"];
+  double epoch = pars["ephepoch"];
+  std::string epoch_time_format = pars["timeformat"];
+  std::string epoch_time_sys = pars["timesys"];
   long num_bins = pars["numbins"];
   std::string time_field = pars["timefield"];
   bool plot = pars["plot"];
   std::string title = pars["title"];
   bool cancel_pdot = pars["cancelpdot"];
   std::string demod_binary_string = pars["demodbin"];
+
+  for (std::string::iterator itor = epoch_time_format.begin(); itor != epoch_time_format.end(); ++itor) *itor = toupper(*itor);
+  if (epoch_time_format != "GLAST") throw std::runtime_error("Epoch may only be in GLAST time format");
 
   using namespace pulsarDb;
 
@@ -122,6 +137,15 @@ void PSearchApp::run() {
   std::string demod_bin_string = pars["demodbin"];
   for (std::string::iterator itor = demod_bin_string.begin(); itor != demod_bin_string.end(); ++itor) *itor = toupper(*itor);
   
+  std::auto_ptr<AbsoluteTime> abs_epoch(0);
+  if (epoch_time_sys == "TDB") {
+    abs_epoch.reset(new GlastTdbTime(epoch));
+  } else if (epoch_time_sys == "TT") {
+    abs_epoch.reset(new GlastTtTime(epoch));
+  } else {
+    throw std::runtime_error("Ephemeris epoch can only be in TDB or TT time systems for now");
+  }
+
   // Open the test file.
   std::auto_ptr<const tip::Table> event_table(tip::IFileSvc::instance().readTable(event_file, event_extension));
 
@@ -131,33 +155,33 @@ void PSearchApp::run() {
   // Get necessary keywords.
   double mjdref = 0.;
   double duration = 0.;
-  double valid_since = 0.;
-  double valid_until = 0.;
+  double tstart = 0.;
+  double tstop = 0.;
   std::string telescope;
-  std::string time_sys;
+  std::string event_time_sys;
   const tip::Header & header(gti_table->getHeader());
   header["MJDREF"].get(mjdref);
   header["TELAPSE"].get(duration);
-  header["TSTART"].get(valid_since);
-  header["TSTOP"].get(valid_until);
+  header["TSTART"].get(tstart);
+  header["TSTOP"].get(tstop);
   header["TELESCOP"].get(telescope);
-  header["TIMESYS"].get(time_sys);
+  header["TIMESYS"].get(event_time_sys);
 
   if (telescope != "GLAST") throw std::runtime_error("Only GLAST supported for now");
 
-  std::auto_ptr<AbsoluteTime> abs_epoch(0);
-  std::auto_ptr<AbsoluteTime> abs_valid_since(0);
-  std::auto_ptr<AbsoluteTime> abs_valid_until(0);
-  if (time_sys == "TDB") {
-    abs_epoch.reset(new GlastTdbTime(epoch));
-    abs_valid_since.reset(new GlastTdbTime(valid_since));
-    abs_valid_until.reset(new GlastTdbTime(valid_until));
-  } else if (time_sys == "TT") {
-    abs_epoch.reset(new GlastTtTime(epoch));
-    abs_valid_since.reset(new GlastTtTime(valid_since));
-    abs_valid_until.reset(new GlastTtTime(valid_until));
+  std::auto_ptr<AbsoluteTime> abs_tstart(0);
+  std::auto_ptr<AbsoluteTime> abs_tstop(0);
+  std::auto_ptr<AbsoluteTime> abs_middle(0);
+  if (event_time_sys == "TDB") {
+    abs_tstart.reset(new GlastTdbTime(tstart));
+    abs_tstop.reset(new GlastTdbTime(tstop));
+    abs_middle.reset(new GlastTdbTime(.5 * (tstart + tstop)));
+  } else if (event_time_sys == "TT") {
+    abs_tstart.reset(new GlastTtTime(tstart));
+    abs_tstop.reset(new GlastTtTime(tstop));
+    abs_middle.reset(new GlastTtTime(.5 * (tstart + tstop)));
   } else {
-    throw std::runtime_error("Only TDB or TT time systems are supported for now");
+    throw std::runtime_error("Event file can only be in TDB or TT time systems for now");
   }
 
   // Compute frequency step from scan step and the Fourier resolution == 1. / duration.
@@ -191,6 +215,7 @@ void PSearchApp::run() {
 
   // Handle either period or frequency-style input.
   std::string eph_style = pars["ephstyle"];
+  for (std::string::iterator itor = eph_style.begin(); itor != eph_style.end(); ++itor) *itor = toupper(*itor);
   if (eph_style == "FREQ") {
     double f0 = pars["f0"];
     double f1 = pars["f1"];
@@ -201,7 +226,7 @@ void PSearchApp::run() {
     // Override any ephemerides which may have been found in the database with the ephemeris the user provided.
     PulsarEphCont & ephemerides(computer.getPulsarEphCont());
     ephemerides.clear();
-    ephemerides.push_back(FrequencyEph(*abs_valid_since, *abs_valid_until, *abs_epoch, phi0, f0, f1, f2).clone());
+    ephemerides.push_back(FrequencyEph(*abs_tstart, *abs_tstop, *abs_epoch, phi0, f0, f1, f2).clone());
   } else if (eph_style == "PER") {
     double p0 = pars["p0"];
     double p1 = pars["p1"];
@@ -212,9 +237,42 @@ void PSearchApp::run() {
     // Override any ephemerides which may have been found in the database with the ephemeris the user provided.
     PulsarEphCont & ephemerides(computer.getPulsarEphCont());
     ephemerides.clear();
-    ephemerides.push_back(PeriodEph(*abs_valid_since, *abs_valid_until, *abs_epoch, phi0, p0, p1, p2).clone());
+    ephemerides.push_back(PeriodEph(*abs_tstart, *abs_tstop, *abs_epoch, phi0, p0, p1, p2).clone());
   } else if (eph_style == "DB") {
     // No action needed.
+  }
+
+  // Handle styles of origin input.
+  std::string origin_style = pars["originstyle"];
+  for (std::string::iterator itor = origin_style.begin(); itor != origin_style.end(); ++itor) *itor = toupper(*itor);
+  std::auto_ptr<AbsoluteTime> abs_origin(0);
+  if (origin_style == "AUTO") {
+    // If user supplied eph epoch manually, use it. Otherwise, use the center of the observation for the
+    // origin of the periodicity test.
+    if (eph_style != "DB") abs_origin.reset(abs_epoch->clone());
+    else abs_origin.reset(abs_middle->clone());
+  } else if (origin_style == "TSTART") {
+    abs_origin.reset(abs_tstart->clone());
+  } else if (origin_style == "TSTOP") {
+    abs_origin.reset(abs_tstop->clone());
+  } else if (origin_style == "MIDDLE") {
+    abs_origin.reset(abs_middle->clone());
+  } else if (origin_style == "USER") {
+    double origin_time = pars["origintime"];
+    std::string origin_time_format = pars["originformat"];
+    for (std::string::iterator itor = origin_time_format.begin(); itor != origin_time_format.end(); ++itor) *itor = toupper(*itor);
+    if (origin_time_format != "GLAST") throw std::runtime_error("Time origin may only be in GLAST time format");
+    std::string origin_time_sys = pars["originsys"];
+    for (std::string::iterator itor = origin_time_sys.begin(); itor != origin_time_sys.end(); ++itor) *itor = toupper(*itor);
+    if (origin_time_sys == "TDB") {
+      abs_origin.reset(new GlastTdbTime(origin_time));
+    } else if (origin_time_sys == "TT") {
+      abs_origin.reset(new GlastTtTime(origin_time));
+    } else {
+      throw std::runtime_error("Time origin may only be in TDB or TT time systems");
+    }
+  } else {
+    throw std::runtime_error("Unsupported origin style " + origin_style);
   }
 
   // Choose which kind of test to create.
@@ -222,16 +280,27 @@ void PSearchApp::run() {
 
   for (std::string::iterator itor = algorithm.begin(); itor != algorithm.end(); ++itor) *itor = toupper(*itor);
 
-  // Compute an ephemeris at abs_epoch to use for the test.
-  std::auto_ptr<PulsarEph> eph(computer.calcPulsarEph(*abs_epoch).clone());
+  // Compute an ephemeris at abs_origin to use for the test.
+  std::auto_ptr<PulsarEph> eph(computer.calcPulsarEph(*abs_origin).clone());
+
+  // Convert absolute origin to the time system demanded by event file.
+  // TODO: Eliminate need for this hideous block.
+  double origin = 0.;
+  if (event_time_sys == "TDB") {
+    origin = GlastTdbTime(*abs_origin).elapsed();
+  } else if (event_time_sys == "TT") {
+    origin = GlastTtTime(*abs_origin).elapsed();
+  } else {
+    throw std::runtime_error("Event file can only be in TDB or TT time systems for now");
+  }
 
   // Create the proper test.
   if (algorithm == "CHI2") 
-    m_test = new ChiSquaredTest(eph->f0(), f_step, num_trials, epoch, num_bins, duration);
+    m_test = new ChiSquaredTest(eph->f0(), f_step, num_trials, origin, num_bins, duration);
   else if (algorithm == "H") 
-    m_test = new HTest(eph->f0(), f_step, num_trials, epoch, num_bins, duration);
+    m_test = new HTest(eph->f0(), f_step, num_trials, origin, num_bins, duration);
   else if (algorithm == "Z2N") 
-    m_test = new Z2nTest(eph->f0(), f_step, num_trials, epoch, num_bins, duration);
+    m_test = new Z2nTest(eph->f0(), f_step, num_trials, origin, num_bins, duration);
   else throw std::runtime_error("PSearchApp: invalid test algorithm " + algorithm);
 
   // Iterate over table, filling the search/test object with temporal data.
@@ -239,7 +308,7 @@ void PSearchApp::run() {
     // Get value from the table.
     double evt_time = (*itor)[time_field].get();
 
-    if (time_sys == "TDB") {
+    if (event_time_sys == "TDB") {
       GlastTdbTime tdb(evt_time);
       // Perform binary correction if so desired.
       if (demod_bin) computer.demodulateBinary(tdb);
@@ -293,10 +362,14 @@ void PSearchApp::prompt(st_app::AppParGroup & pars) {
 
   std::string eph_style = pars["ephstyle"];
   if (eph_style == "FREQ") {
-    pars.Prompt("epoch");
+    pars.Prompt("ephepoch");
+    pars.Prompt("timeformat");
+    pars.Prompt("timesys");
     pars.Prompt("f0");
   } else if (eph_style == "PER") {
-    pars.Prompt("epoch");
+    pars.Prompt("ephepoch");
+    pars.Prompt("timeformat");
+    pars.Prompt("timesys");
     pars.Prompt("p0");
   } else if (eph_style == "DB") {
     // No action needed.
@@ -316,6 +389,13 @@ void PSearchApp::prompt(st_app::AppParGroup & pars) {
       pars.Prompt("p2");
     }
     // if eph_style == "DB", coeffs will be determined.
+  }
+
+  std::string origin_style = pars["originstyle"];
+  if (origin_style == "USER") {
+    pars.Prompt("origintime");
+    pars.Prompt("originformat");
+    pars.Prompt("originsys");
   }
 
   pars.Prompt("numtrials");
