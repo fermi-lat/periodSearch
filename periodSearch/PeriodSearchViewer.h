c38625114d144f68d8a71662fa6e48dada80dfba
peachey <>
2007-03-07 20:44:45 +0000

    Add viewer class, which can plot and also write output.

---
+++ periodSearch/PeriodSearchViewer.h
@@ -0,0 +1,54 @@
+/** \file PeriodSearchViewer.h
+    \brief Declaration of PeriodSearchViewer class.
+    \authors Masaharu Hirayama, GSSC
+             James Peachey, HEASARC/GSSC
+*/
+#ifndef periodSearch_PeriodSearchViewer_h
+#define periodSearch_PeriodSearchViewer_h
+
+#include <string>
+
+namespace st_stream {
+  class OStream;
+}
+
+namespace periodSearch {
+
+  class PeriodSearch;
+
+  /** \class PeriodSearchViewer
+      \brief Base class for various statistical tests used to determine frequency of pulsation
+             when an approximate frequency is known.
+  */
+  class PeriodSearchViewer {
+    public:
+      /** \brief Create a viewer object for viewing period search objects (plotting and/or writing output).
+          \param search The search being viewed.
+          \param min_freq The minimum frequency in the range (if negative, do not constrain minimum frequency.)
+          \param max_freq The maximum frequency in the range (if negative, do not constrain maximum frequency.)
+      */
+      PeriodSearchViewer(const PeriodSearch & search, double min_freq = -1., double max_freq = -1.);
+
+      virtual ~PeriodSearchViewer();
+
+      /** \brief Display plot of statistic as a function of frequency for entire frequency range.
+          \param title The title to display on the plot. (Purely cosmetic.)
+          \param freq_unit The units to display on the x axis. (Purely cosmetic.)
+      */
+      virtual void plot(const std::string & title, const std::string & freq_unit) const;
+
+      /** \brief Write description of this test to the given stream.
+          \param os The stream to which to send output.
+      */
+      virtual st_stream::OStream & write(st_stream::OStream & os) const;
+
+    private:
+      const PeriodSearch * m_search;
+      double m_min_freq;
+      double m_max_freq;
+  };
+
+  st_stream::OStream & operator <<(st_stream::OStream & os, const PeriodSearchViewer & viewer);
+}
+
+#endif
---
+++ src/PeriodSearchViewer.cxx
@@ -0,0 +1,121 @@
+/** \file PeriodSearchViewer.cxx
+    \brief Implementation of PeriodSearchViewer class.
+    \authors Masaharu Hirayama, GSSC
+             James Peachey, HEASARC/GSSC
+*/
+
+#include "periodSearch/PeriodSearch.h"
+#include "periodSearch/PeriodSearchViewer.h"
+
+#include "st_graph/Axis.h"
+#include "st_graph/Engine.h"
+#include "st_graph/IPlot.h"
+#include "st_graph/Sequence.h"
+
+#include "st_stream/Stream.h"
+
+#include <iomanip>
+#include <iostream>
+#include <memory>
+#include <sstream>
+#include <stdexcept>
+#include <utility>
+#include <vector>
+
+namespace periodSearch {
+
+  PeriodSearchViewer::PeriodSearchViewer(const PeriodSearch & search, double min_freq, double max_freq):
+    m_search(&search), m_min_freq(min_freq), m_max_freq(max_freq) {}
+
+  PeriodSearchViewer::~PeriodSearchViewer() {}
+
+  void PeriodSearchViewer::plot(const std::string & title, const std::string & freq_unit) const {
+    using namespace st_graph;
+    typedef PeriodSearch::cont_type cont_type;
+    typedef PeriodSearch::size_type size_type;
+
+    try {
+      // Display value of maximum frequency/statistic in title.
+      std::ostringstream os;
+      std::pair<double, double> max = m_search->findMax(m_min_freq, m_max_freq);
+      std::pair<double, double> chance_prob = m_search->chanceProb(max.second);
+
+      os << title << ", max at: " << max.first << ", stat: " << max.second;
+
+      // Massage display: if difference between min and max is small enough just use max.
+      os.setf(std::ios::scientific);
+      os.precision(2); // 3 digits -> < 1. e -4. limit in next line.
+      if ((chance_prob.second - chance_prob.first) / chance_prob.second < 1.e-4)
+        os << ", chance prob: " << chance_prob.second;
+      else
+        os << ", chance prob < " << chance_prob.second;
+
+      // Get graphics engine to set up graph.
+      Engine & engine(Engine::instance());
+
+      // Typedef for readability.
+      typedef st_graph::ValueSequence<std::vector<double>::const_iterator> ValueSeq_t;
+
+      // Impose range limits.
+      // TODO Move interpretation of min/max frequency into constructor and store the selected index locating
+      // the min and max to avoid recomputing min/max index each time this is called.
+      std::pair<size_type, size_type> indices = m_search->getRangeIndex(m_min_freq, m_max_freq);
+      size_type begin_index = indices.first;
+      size_type end_index = indices.second;
+
+      // Create plot, using frequency as x, and spectrum/statistic as y.
+      const cont_type & freq(m_search->getFreq());
+      const cont_type & spec(m_search->getSpec());
+      std::auto_ptr<IPlot> plot(engine.createPlot(os.str(), 800, 600, "hist",
+        ValueSeq_t(freq.begin() + begin_index, freq.begin() + end_index),
+        ValueSeq_t(spec.begin() + begin_index, spec.begin() + end_index)));
+
+      // Set axes titles.
+      std::vector<Axis> & axes(plot->getAxes());
+      axes[0].setTitle("Frequency " + freq_unit);
+      axes[1].setTitle("Test Statistic");
+
+      // Display plot.
+      engine.run();
+
+    } catch (const std::exception & x) {
+      std::cerr << x.what() << std::endl;
+      std::cerr << "Warning: PeriodSearchViewer::plot could not display plot." << std::endl;
+    }
+  }
+
+  st_stream::OStream & PeriodSearchViewer::write(st_stream::OStream & os) const {
+    using namespace std;
+
+    // Save current precision.
+    int save_precision = os.precision();
+
+    os.precision(15);
+
+    // Write out the data.
+    os << "Frequency\tStatistic";
+
+    // Impose range limits.
+    // TODO Move interpretation of min/max frequency into constructor and store the selected index locating
+    // the min and max to avoid recomputing min/max index each time this is called.
+    std::pair<PeriodSearch::size_type, PeriodSearch::size_type> indices = m_search->getRangeIndex(m_min_freq, m_max_freq);
+    PeriodSearch::size_type begin_index = indices.first;
+    PeriodSearch::size_type end_index = indices.second;
+
+    const std::vector<double> & freq(m_search->getFreq());
+    const std::vector<double> & spec(m_search->getSpec());
+
+    // Write out the statistics.
+    for (PeriodSearch::size_type ii = begin_index; ii < end_index; ++ii) os << std::endl << freq[ii] << "\t" << spec[ii];
+
+    // Restore original precision.
+    os.precision(save_precision);
+
+    return os;
+  }
+
+  st_stream::OStream & operator <<(st_stream::OStream & os, const PeriodSearchViewer & viewer) {
+    return viewer.write(os);
+  }
+
+}
