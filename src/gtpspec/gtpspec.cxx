2ae844e0feff772b9712f9ef92bfa139a746ea07
hirayama <>
2007-09-17 21:44:12 +0000

    Add an EventTimeHandlerFactory object to support GLAST LAT data.

--- src/gtpspec/gtpspec.cxx
+++ src/gtpspec/gtpspec.cxx
@@ -4,6 +4,13 @@
             James Peachey, HEASARC/GSSC
 */
 #include "PowerSpectrumApp.h"
+
 #include "st_app/StAppFactory.h"
+
+#include "timeSystem/EventTimeHandler.h"
+#include "timeSystem/GlastTimeHandler.h"
+
+// List supported mission(s).
+timeSystem::EventTimeHandlerFactory<timeSystem::GlastTimeHandler> glast_handler;
 
 st_app::StAppFactory<PowerSpectrumApp> g_factory("gtpspec");
