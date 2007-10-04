8abda72a2499c9be7e727981abe2937c23363d67
hirayama <>
2007-10-04 21:58:03 +0000

    Clean up headers and namespaces for st_facilities and facilities.

--- src/PowerSpectrumApp.cxx
+++ src/PowerSpectrumApp.cxx
@@ -11,6 +11,8 @@
 #include <string>
 #include <vector>
 
+#include "facilities/commonUtilities.h"
+
 #include "pulsarDb/EphChooser.h"
 #include "pulsarDb/TimingModel.h"
 
@@ -21,10 +23,6 @@
 #include "tip/Table.h"
 
 #include "st_app/AppParGroup.h"
-
-#include "st_facilities/Env.h"
-
-#include "facilities/commonUtilities.h"
 
 #include "st_stream/Stream.h"
 #include "st_stream/StreamFormatter.h"
@@ -135,8 +133,8 @@ void PowerSpectrumApp::run() {
 
   if ("NONE" != out_file_uc) {
     // Find the template file.
-    using namespace st_facilities;
-    std::string template_file = facilities::commonUtilities::joinPath(facilities::commonUtilities::getDataPath("periodSearch"), "period-search-out.tpl");
+    using namespace facilities;
+    std::string template_file = commonUtilities::joinPath(commonUtilities::getDataPath("periodSearch"), "period-search-out.tpl");
 
     // Create output file.
     tip::IFileSvc::instance().createFile(out_file, template_file, clobber);
