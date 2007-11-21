4f7d1a09707bea93e789a53d0bd214a77f888c91
hirayama <>
2007-11-21 22:25:20 +0000

    Add plot method. Fix the signature of size method.

--- src/ChiSquaredTestArray.cxx
+++ src/ChiSquaredTestArray.cxx
@@ -3,11 +3,19 @@
     \authors Masaharu Hirayama, GSSC
              James Peachey, HEASARC/GSSC
 */
+#include "ChiSquaredTestArray.h"
+
+#include <iostream>
 #include <sstream>
 #include <stdexcept>
+#include <vector>
+
+#include "st_graph/Axis.h"
+#include "st_graph/Engine.h"
+#include "st_graph/IPlot.h"
+#include "st_graph/Sequence.h"
 
 #include "ChiSquaredProb.h"
-#include "ChiSquaredTestArray.h"
 
 ChiSquaredTestArray::ChiSquaredTestArray(size_type array_size, data_type::size_type num_phase_bins):
   m_num_phase_bins(num_phase_bins), m_curve_cont(array_size, data_type(num_phase_bins, 0)), m_num_events(0) {}
@@ -65,4 +73,43 @@ std::string ChiSquaredTestArray::getDescription() const {
 
 ChiSquaredTestArray::size_type ChiSquaredTestArray::size() const {
   return m_curve_cont.size();
+}
+
+void ChiSquaredTestArray::plot(const std::string & title, size_type array_index) const {
+  using namespace st_graph;
+
+  // Create a light curve to plot.
+  const data_type & curve = m_curve_cont.at(array_index);
+  typedef std::vector<double> hist_type;
+  hist_type phase_value(m_num_phase_bins, 0.);
+  hist_type light_curve(m_num_phase_bins, 0.);
+  for (hist_type::size_type ii=0; ii < hist_type::size_type(m_num_phase_bins); ++ii) {
+    phase_value[ii] = (ii + 0.5) / m_num_phase_bins;
+    light_curve[ii] = curve[ii];
+  }
+
+  try {
+    // Get graphics engine to set up graph.
+    Engine & engine(Engine::instance());
+
+    // Typedef for readability.
+    typedef st_graph::ValueSequence<std::vector<double>::const_iterator> ValueSeq_t;
+
+    // TODO Add text output (statistics, etc.)to a text box on the plot, and/or in a GUI output window.
+    std::auto_ptr<IPlot> plot(engine.createPlot(title, 800, 600, "hist",
+      ValueSeq_t(phase_value.begin(), phase_value.end()),
+      ValueSeq_t(light_curve.begin(), light_curve.end())));
+
+    // Set axes titles.
+    std::vector<Axis> & axes(plot->getAxes());
+    axes[0].setTitle("Pulse Phase");
+    axes[1].setTitle("Counts");
+
+    // Display plot.
+    engine.run();
+
+  } catch (const std::exception & x) {
+    std::cerr << x.what() << std::endl;
+    std::cerr << "Warning: ChiSquaredTestArray::plot could not display plot." << std::endl;
+  }
 }
