480ff39a9323bab97d203187a989079c983e0e31
hirayama <>
2008-10-05 03:13:31 +0000

    Split application file into implementation and definition.

---
+++ src/PeriodSearchApp.h
@@ -0,0 +1,27 @@
+/** \file PeriodSearchApp.h
+    \brief Declaration of PeriodSearchApp class.
+    \author Masaharu Hirayama, GSSC
+            James Peachey, HEASARC/GSSC
+*/
+#ifndef periodSearch_PeriodSearchApp_h
+#define periodSearch_PeriodSearchApp_h
+
+#include "pulsarDb/PulsarToolApp.h"
+
+#include "st_app/AppParGroup.h"
+
+#include "st_stream/StreamFormatter.h"
+
+class PeriodSearchApp : public pulsarDb::PulsarToolApp {
+  public:
+    PeriodSearchApp();
+    virtual ~PeriodSearchApp() throw();
+    virtual void run();
+
+  private:
+    st_stream::StreamFormatter m_os;
+
+    virtual void prompt(st_app::AppParGroup & pars);
+};
+
+#endif
